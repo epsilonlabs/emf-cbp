d7b81210999b77d0b6cedd4831f5f0161b57f3f8
dkolovos <dkolovos@cs.york.ac.uk>
2017-02-19 18:04:24 +0000

    Added a few more tests for enumeration-typed attributes

--- org.eclipse.epsilon.cbp.test/src/org/eclipse/epsilon/cbp/test/BlogEquivalenceTests.java
+++ org.eclipse.epsilon.cbp.test/src/org/eclipse/epsilon/cbp/test/BlogEquivalenceTests.java
@@ -8,13 +8,33 @@ public class BlogEquivalenceTests extends XmiResourceEquivalenceTests {
 
 	@Test
 	public void addRatings() throws Exception {
-		run("var p : new Post; p.ratings.add(1); p.ratings.add(2);", true);
+		run("var p : new Post; p.ratings.add(1); p.ratings.add(2);");
+	}
+	
+	@Test
+	public void setPostType() throws Exception {
+		run("var p : new Post; p.type = PostType#Sticky;");		
+	}
+	
+	@Test
+	public void unsetPostType() throws Exception {
+		run("var p : new Post; p.type = PostType#Sticky; p.type = null;");		
 	}
 	
 	@Test
 	public void removeRatings() throws Exception {
-		run("var p : new Post; p.ratings.add(1); p.ratings.add(2); p.ratings.remove(1);", true);		
+		run("var p : new Post; p.ratings.add(1); p.ratings.add(2); p.ratings.remove(1);");		
 	}
+	
+	@Test
+	public void addCommentFlag() throws Exception {
+		run("var c : new Comment; c.flags.add(Flag#Offensive);", true);
+	}
+
+	@Test
+	public void revmoveCommentFlag() throws Exception {
+		run("var c : new Comment; c.flags.add(Flag#Offensive); c.flags.remove(Flag#Offensive);", true);
+	}	
 	
 	@Override
 	public EPackage getEPackage() {
