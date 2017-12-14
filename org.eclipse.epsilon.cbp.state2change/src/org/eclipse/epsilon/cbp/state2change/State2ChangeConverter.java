db259d6c86d2a52f0075164ded5a39820787d7c8
Alfa Yohannis <ary506@york.ac.uk>
2017-12-14 10:28:38 +0000

    Optimise Code to save up more memory consumed by UML2 CacheAdapater
    Optimise Code to save up more memory consumed by UML2 CacheAdapater

--- org.eclipse.epsilon.cbp.state2change/src/org/eclipse/epsilon/cbp/state2change/State2ChangeConverter.java
+++ org.eclipse.epsilon.cbp.state2change/src/org/eclipse/epsilon/cbp/state2change/State2ChangeConverter.java
@@ -1,9 +1,11 @@
 package org.eclipse.epsilon.cbp.state2change;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
@@ -12,16 +14,10 @@ import org.eclipse.emf.common.util.BasicMonitor;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.compare.AttributeChange;
 import org.eclipse.emf.compare.Comparison;
 import org.eclipse.emf.compare.Diff;
 import org.eclipse.emf.compare.EMFCompare;
 import org.eclipse.emf.compare.EMFCompare.Builder;
-import org.eclipse.emf.compare.FeatureMapChange;
-import org.eclipse.emf.compare.ReferenceChange;
-import org.eclipse.emf.compare.ResourceAttachmentChange;
-import org.eclipse.emf.compare.ResourceLocationChange;
-import org.eclipse.emf.compare.internal.spec.ReferenceChangeSpec;
 import org.eclipse.emf.compare.merge.BatchMerger;
 import org.eclipse.emf.compare.merge.IBatchMerger;
 import org.eclipse.emf.compare.merge.IMerger;
@@ -29,33 +25,15 @@ import org.eclipse.emf.compare.postprocessor.BasicPostProcessorDescriptorImpl;
 import org.eclipse.emf.compare.postprocessor.IPostProcessor;
 import org.eclipse.emf.compare.postprocessor.PostProcessorDescriptorRegistryImpl;
 import org.eclipse.emf.compare.scope.DefaultComparisonScope;
-import org.eclipse.emf.compare.scope.IComparisonScope;
 import org.eclipse.emf.compare.scope.IComparisonScope2;
-import org.eclipse.emf.compare.uml2.internal.AssociationChange;
-import org.eclipse.emf.compare.uml2.internal.DirectedRelationshipChange;
-import org.eclipse.emf.compare.uml2.internal.ExecutionSpecificationChange;
-import org.eclipse.emf.compare.uml2.internal.ExtendChange;
-import org.eclipse.emf.compare.uml2.internal.GeneralizationSetChange;
-import org.eclipse.emf.compare.uml2.internal.IntervalConstraintChange;
-import org.eclipse.emf.compare.uml2.internal.MessageChange;
-import org.eclipse.emf.compare.uml2.internal.MultiplicityElementChange;
-import org.eclipse.emf.compare.uml2.internal.OpaqueElementBodyChange;
-import org.eclipse.emf.compare.uml2.internal.ProfileApplicationChange;
-import org.eclipse.emf.compare.uml2.internal.StereotypeApplicationChange;
-import org.eclipse.emf.compare.uml2.internal.StereotypeAttributeChange;
-import org.eclipse.emf.compare.uml2.internal.StereotypeReferenceChange;
-import org.eclipse.emf.compare.uml2.internal.StereotypedElementChange;
 import org.eclipse.emf.compare.uml2.internal.merge.UMLMerger;
 import org.eclipse.emf.compare.uml2.internal.postprocessor.UMLPostProcessor;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.impl.EStructuralFeatureImpl.InverseUpdatingFeatureMapEntry;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.util.ECrossReferenceAdapter;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.XMIResource;
-import org.eclipse.emf.ecore.xmi.impl.XMILoadImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
 import org.eclipse.epsilon.cbp.resource.CBPResource;
@@ -66,7 +44,28 @@ import org.eclipse.uml2.uml.UMLPackage;
 
 import com.google.common.collect.ImmutableSet;
 
+@SuppressWarnings("restriction")
 public class State2ChangeConverter {
+
+	protected URI rootXmiFileUri;
+	protected URI commonXmiURI = URI.createURI("umlmodel.xmi");
+	protected URI commonCbpxmlURI = URI.createURI("umlmodel.cbpxml");
+
+	protected IBatchMerger batchMerger;
+	protected EMFCompare comparator;
+
+	protected List<ResourceSet> resourceSetList = new ArrayList<>();
+
+	protected Resource cbpResource;
+	protected Resource cbpStateResource;
+	protected ResourceSet cbpResourceSet;
+	protected ResourceSet xmiResourceSet;
+	protected ResourceSet comparisonResourceSet;
+	protected ResourceSet cbpTestResourceSet;
+	protected ResourceSet cbpStateResourceSet;
+
+	protected Map<Object, Object> saveOptions;
+	protected String cbpStatePath;
 
 	private File sourceDirectory;
 
@@ -77,74 +76,47 @@ public class State2ChangeConverter {
 		this.sourceDirectory = xmiDirectory;
 	}
 
-	public boolean generate(File cbpFile, File diffDirectory) throws Exception {
-		Resource cbpResource;
-		Resource rootXmiResource = null;
-		File rootXmiFile;
-		ResourceSet rootXmiResourceSet = null;
-		String rootXmiFilePath;
-		URI rootXmiFileUri;
-		URI commonXmiURI = URI.createURI("umlmodel.xmi");
-		URI commonCbpxmlURI = URI.createURI("umlmodel.cbpxml");
+	public class Task extends Thread {
 
-		UMLPackage.eINSTANCE.eClass();
-		// JavaPackage.eINSTANCE.eClass();
-		final Map<Object, Object> saveOptions = (new XMIResourceImpl()).getDefaultSaveOptions();
-		saveOptions.put(XMIResource.OPTION_PROCESS_DANGLING_HREF, XMIResource.OPTION_PROCESS_DANGLING_HREF_RECORD);
+		protected File cbpFile;
+		protected File diffDirectory;
+		protected File xmiFile;
 
-		// -----
+		public Task(File cbpFile, File diffDirectory, File sourceFile) throws IOException {
+			super();
+			this.cbpFile = cbpFile;
+			this.diffDirectory = diffDirectory;
+			this.xmiFile = sourceFile;
 
-		ResourceSet cbpResourceSet = new ResourceSetImpl();
-		cbpResourceSet.getResourceFactoryRegistry().getContentTypeToFactoryMap().put("cbpxml",
-				new CBPXMLResourceFactory());
-		cbpResource = new CBPXMLResourceImpl();
-		URI cbpUri = URI.createFileURI(cbpFile.getAbsolutePath());
+		}
 
-		// File file = new File(cbpUri.toFileString());
-		// if (file.exists()) {
-		// file.delete();
-		// }
-		cbpResource.setURI(cbpUri);
-		cbpResourceSet.getResources().add(cbpResource);
-		cbpResource.load(null);
+		@SuppressWarnings("restriction")
+		@Override
+		public void run() {
 
-		IPostProcessor.Descriptor.Registry<String> postProcessorRegistry = new PostProcessorDescriptorRegistryImpl<String>();
-		BasicPostProcessorDescriptorImpl post = new BasicPostProcessorDescriptorImpl(new UMLPostProcessor(),
-				Pattern.compile("http://www.eclipse.org/uml2/5.0.0/UML"), null);
-		postProcessorRegistry.put(UMLPostProcessor.class.getName(), post);
-		Builder builder = EMFCompare.builder();
-		builder.setPostProcessorRegistry(postProcessorRegistry);
-		EMFCompare comparator = builder.build();
-
-		IMerger.Registry registry = IMerger.RegistryImpl.createStandaloneInstance();
-		UMLMerger umlMerger = new UMLMerger();
-		umlMerger.setRanking(11);
-		registry.add(umlMerger);
-		IBatchMerger batchMerger = new BatchMerger(registry);
-		// -----
-
-		ResourceSet xmiResourceSet = new ResourceSetImpl();
-		xmiResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
-		ResourceSet comparisonResourceSet = new ResourceSetImpl();
-		comparisonResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi",
-				new XMIResourceFactoryImpl());
-		ResourceSet cbpTestResourceSet = new ResourceSetImpl();
-		cbpTestResourceSet.getResourceFactoryRegistry().getContentTypeToFactoryMap().put("cbpxml",
-				new CBPXMLResourceFactory());
-		ResourceSet cbpStateResourceSet = new ResourceSetImpl();
-		cbpStateResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi",
-				new XMIResourceFactoryImpl());
-		String cbpStatePath = "D:/TEMP/BigModel/cbp-state/";
-		Resource cbpStateResource = cbpStateResourceSet.createResource(URI.createURI("cbp-state.xmi"));
-
-		File[] sourceFiles = sourceDirectory.listFiles();
-		System.out.println("Converting " + sourceFiles.length + " file(s) to CBP");
-
-		for (int i = 0; i < sourceFiles.length; i++) {
 			System.gc();
-			System.out.print("Converting file " + sourceFiles[i].getName() + " to CBP");
+			System.out.print("Converting file " + xmiFile.getName() + " to CBP");
 
-			File xmiFile = sourceFiles[i];
+			try {
+				cbpResource.load(null);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			IPostProcessor.Descriptor.Registry<String> postProcessorRegistry = new PostProcessorDescriptorRegistryImpl<String>();
+			BasicPostProcessorDescriptorImpl post = new BasicPostProcessorDescriptorImpl(new UMLPostProcessor(),
+					Pattern.compile("http://www.eclipse.org/uml2/5.0.0/UML"), null);
+			postProcessorRegistry.put(UMLPostProcessor.class.getName(), post);
+			Builder builder = EMFCompare.builder();
+			builder.setPostProcessorRegistry(postProcessorRegistry);
+			comparator = builder.build();
+
+			IMerger.Registry registry = IMerger.RegistryImpl.createStandaloneInstance();
+			UMLMerger umlMerger = new UMLMerger();
+			umlMerger.setRanking(11);
+			registry.add(umlMerger);
+			batchMerger = new BatchMerger(registry);
 
 			String xmiFilePath = xmiFile.getAbsolutePath();
 			URI xmiFileUri = URI.createFileURI(xmiFilePath);
@@ -163,41 +135,51 @@ public class State2ChangeConverter {
 			comparisonResource.getContents().addAll(EcoreUtil.copyAll(comparison.getDifferences()));
 			String diffFilePath = diffDirectory.getPath() + File.separator + "Diff-" + xmiFile.getName();
 			File diffFile = new File(diffFilePath);
-			FileOutputStream diffFileOutputStream = new FileOutputStream(diffFile);
-			comparisonResource.save(diffFileOutputStream, saveOptions);
-			clearCacheAdapter(comparisonResource);
-			comparisonResource.getContents().clear();
-			comparisonResource.eAdapters().clear();
-			comparisonResource.unload();
-			CacheAdapter.getInstance().clear(comparisonResource);
-			diffFileOutputStream.flush();
-			diffFileOutputStream.close();
+			FileOutputStream diffFileOutputStream;
+			try {
+				diffFileOutputStream = new FileOutputStream(diffFile);
+				comparisonResource.save(diffFileOutputStream, saveOptions);
+				diffFileOutputStream.flush();
+				diffFileOutputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 
 			// copy all right to left
+			List<EObject> list = new ArrayList<EObject>();
+			TreeIterator<EObject> iterator = cbpResource.getAllContents();
+			while (iterator.hasNext()) {
+				EObject eObject = iterator.next();
+				list.add(eObject);
+			}
 			CBPXMLResourceImpl cbpImpl = ((CBPXMLResourceImpl) cbpResource);
 			cbpImpl.startNewSession(xmiFile.getName());
 			System.out.print(" " + diffs.size());
 			batchMerger.copyAllRightToLeft(diffs, new BasicMonitor());
-			cbpResource.save(null);
+			try {
+				cbpResource.save(null);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 
-			// //---test by reload
-			((CBPResource) cbpResource).getModelHistory().clear();
-			((CBPResource) cbpResource).getIgnoreSet().clear();
-			cbpResource.getContents().clear();
-			cbpResource.unload();
-			cbpResource.load(null);
+//			// //---test by reload
+//			((CBPResource) cbpResource).getModelHistory().clear();
+//			((CBPResource) cbpResource).getIgnoreSet().clear();
+//			clearCacheAdapter(cbpResource);
+//			try {
+//				cbpResource.load(null);
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
 
 			cbpStateResource.getContents().addAll(EcoreUtil.copyAll(cbpResource.getContents()));
 			URI cbpStateFileUri = URI.createFileURI(cbpStatePath + "Cbp-state-" + xmiFile.getName());
 			cbpStateResource.setURI(cbpStateFileUri);
-			cbpStateResource.save(null);
-			clearCacheAdapter(cbpStateResource);
-			cbpStateResource.getContents().clear();
-			cbpStateResource.eAdapters().clear();
-			cbpStateResource.unload();
-			CacheAdapter.getInstance().clear(cbpStateResource);
-
-			clearCacheAdapter(cbpResource);
+			try {
+				cbpStateResource.save(null);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 
 			// after merging, to check if there are no more differences
 			scope = createComparisonScope(cbpResource, xmiResource);
@@ -208,22 +190,73 @@ public class State2ChangeConverter {
 				System.out.println(diff);
 			}
 			if (diffs.size() > 2) {
-				throw new Exception("There are still differences between the CBP and the XMI.");
+				System.out.println("There are still differences between the CBP and the XMI.");
+				return;
 			}
 
-			clearCacheAdapter(xmiResource);
-			xmiResource.getContents().clear();
-			xmiResource.eAdapters().clear();
-			xmiResource.unload();
-
-			xmiResourceSet.getResources().clear();
-			cbpTestResourceSet.getResources().clear();
-			comparisonResourceSet.getResources().clear();
-			cbpStateResourceSet.getResources().clear();
-
+			clearCacheAdapter();
 			((CBPResource) cbpResource).getModelHistory().clear();
 			((CBPResource) cbpResource).getIgnoreSet().clear();
+			for (EObject eObject: list) {
+				CacheAdapter.getInstance().getInverseReferences(eObject).clear();
+				CacheAdapter.getInstance().getNonNavigableInverseReferences(eObject).clear();
+				eObject.eAdapters().clear();
+				EcoreUtil.remove(eObject);
+			}
+			list.clear();
+			clearCacheAdapter(cbpResource);
+		}
+	}
 
+	public boolean generate(File cbpFile, File diffDirectory) throws Exception {
+
+		UMLPackage.eINSTANCE.eClass();
+		// JavaPackage.eINSTANCE.eClass();
+		saveOptions = (new XMIResourceImpl()).getDefaultSaveOptions();
+		saveOptions.put(XMIResource.OPTION_PROCESS_DANGLING_HREF, XMIResource.OPTION_PROCESS_DANGLING_HREF_RECORD);
+
+		// -----
+		cbpResourceSet = new ResourceSetImpl();
+		cbpResourceSet.getResourceFactoryRegistry().getContentTypeToFactoryMap().put("cbpxml",
+				new CBPXMLResourceFactory());
+
+		cbpResource = new CBPXMLResourceImpl();
+		URI cbpUri = URI.createFileURI(cbpFile.getAbsolutePath());
+		File file = new File(cbpUri.toFileString());
+		if (file.exists()) {
+			file.delete();
+			file.createNewFile();
+		}
+		cbpResource.setURI(cbpUri);
+		cbpResourceSet.getResources().add(cbpResource);
+//		cbpResource.load(null);
+
+		xmiResourceSet = new ResourceSetImpl();
+		xmiResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+		resourceSetList.add(xmiResourceSet);
+
+		comparisonResourceSet = new ResourceSetImpl();
+		comparisonResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi",
+				new XMIResourceFactoryImpl());
+		resourceSetList.add(comparisonResourceSet);
+
+		cbpStateResourceSet = new ResourceSetImpl();
+		cbpStateResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi",
+				new XMIResourceFactoryImpl());
+		resourceSetList.add(cbpStateResourceSet);
+
+		cbpStatePath = "D:/TEMP/BigModel/cbp-state/";
+		cbpStateResource = cbpStateResourceSet.createResource(URI.createURI("cbp-state.xmi"));
+
+		File[] sourceFiles = sourceDirectory.listFiles();
+		System.out.println("Converting " + sourceFiles.length + " file(s) to CBP");
+		for (int i = 0; i < sourceFiles.length; i++) {
+			Task task = new Task(cbpFile, diffDirectory, sourceFiles[i]);
+			task.setDaemon(true);
+			task.setName(sourceFiles[i].getName());
+			task.start();
+			task.join();
+			System.out.println();
 		}
 		System.out.println("Finished!");
 		return true;
@@ -243,15 +276,51 @@ public class State2ChangeConverter {
 		return scope;
 	}
 
-	private void clearCacheAdapter(Resource resource) {
-		TreeIterator<EObject> iterator = resource.getAllContents();
-		while (iterator.hasNext()) {
-			EObject item = iterator.next();
-			CacheAdapter.getInstance().getInverseReferences(item).clear();
-			CacheAdapter.getInstance().getNonNavigableInverseReferences(item).clear();
+	private void clearCacheAdapter() {
+		for (ResourceSet resourceSet : resourceSetList) {
+			for (Resource resource : resourceSet.getResources()) {
+				TreeIterator<EObject> iterator = resource.getAllContents();
+				List<EObject> list = new ArrayList<>();
+				while (iterator.hasNext()) {
+					EObject item = iterator.next();
+					list.add(item);
+				}
+				for (EObject eObject : list) {
+					CacheAdapter.getInstance().getInverseReferences(eObject).clear();
+					CacheAdapter.getInstance().getNonNavigableInverseReferences(eObject).clear();
+					eObject.eAdapters().clear();
+					EcoreUtil.remove(eObject);
+					
+				}
+				list.clear();
+				CacheAdapter.getInstance().clear(resource);
+				CacheAdapter.getInstance().clear();
+
+				resource.getContents().clear();
+				resource.unload();
+			}
+			resourceSet.getResources().clear();
 		}
-		CacheAdapter.getInstance().clear(resource);
-		CacheAdapter.getInstance().clear();
 	}
 
+	private void clearCacheAdapter(Resource resource) {
+		TreeIterator<EObject> iterator = resource.getAllContents();
+		List<EObject> list = new ArrayList<>();
+		while (iterator.hasNext()) {
+			EObject item = iterator.next();
+			list.add(item);
+		}
+		for (EObject eObject : list) {
+			CacheAdapter.getInstance().getInverseReferences(eObject).clear();
+			CacheAdapter.getInstance().getNonNavigableInverseReferences(eObject).clear();
+			eObject.eAdapters().clear();
+			EcoreUtil.remove(eObject);
+			
+		}
+		list.clear();
+		CacheAdapter.getInstance().clear(resource);
+		CacheAdapter.getInstance().clear();
+		resource.getContents().clear();
+		resource.unload();
+	}
 }
